11f7fdd38e3b0aa962ec4d7558dcf4b33cae79ea
Harrison MG <gieraltowski.har@gmail.com>
2023-09-13 16:13:02 -0400

    began work on better cursors

--- crates/client/src/cursors.rs
+++ crates/client/src/cursors.rs
@@ -1,12 +1,7 @@
-use bevy::{
-    prelude::*,
-    render::{mesh::Indices, render_resource::PrimitiveTopology},
-    sprite::MaterialMesh2dBundle,
-    utils::HashMap,
-};
+use bevy::{prelude::*, utils::HashMap};
 use rand::Rng;
 
-use game::{Cursor, PlayerCursorMovedEvent, PlayerDisconnectedEvent, Puzzle, Uuid};
+use game::{Cursor, PlayerCursorMovedEvent, PlayerDisconnectedEvent, Uuid};
 
 use crate::{
     mouse::{WorldCursorMoved, WorldCursorPosition},
@@ -14,14 +9,15 @@ use crate::{
 };
 use crate::{states::AppState, PuzzleComplete};
 
-const CURSOR_SIZE_RATIO: f32 = 0.6;
+const CURSOR_SIZE: f32 = 0.6;
 const CURSOR_HEIGHT: f32 = MAX_PIECE_HEIGHT + 1.0;
 
 pub struct CursorPlugin;
 
 impl Plugin for CursorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::Playing), player_cursors_setup)
+        app.add_systems(Startup, cursors_setup)
+            .add_systems(OnEnter(AppState::Playing), player_cursors_setup)
             .add_systems(
                 Update,
                 (
@@ -31,59 +27,33 @@ impl Plugin for CursorPlugin {
                     cursor_party,
                 )
                     .run_if(in_state(AppState::Playing)),
-            );
+            )
+            .add_systems(LateUpdate, cursor_resize);
     }
 }
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub struct CursorComponent {}
 
-#[derive(Bundle)]
+#[derive(Bundle, Clone)]
 pub struct CursorBundle {
-    cursor: CursorComponent,
-    mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
+    pub cursor: CursorComponent,
+    pub sprite_bundle: SpriteBundle,
 }
 
-fn add_cursor(
-    cursor: &Cursor,
-    player_id: Uuid,
-    puzzle: &Puzzle,
-    meshes: &mut Assets<Mesh>,
-    materials: &mut Assets<ColorMaterial>,
-    cursor_map: &mut CursorMap,
-    commands: &mut Commands,
-) {
-    let cursor_size = puzzle.piece_width().min(puzzle.piece_height()) as f32 * CURSOR_SIZE_RATIO;
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.insert_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vec![
-            [0.0, 0.0, 0.0],
-            [0.0, -cursor_size, 0.0],
-            [
-                cursor_size * std::f32::consts::FRAC_1_SQRT_2,
-                -cursor_size * std::f32::consts::FRAC_1_SQRT_2,
-                0.0,
-            ],
-        ],
-    );
-    mesh.set_indices(Some(Indices::U32(vec![0, 1, 2])));
-    let mesh_handle = meshes.add(mesh);
+#[derive(Resource)]
+pub struct CursorPrefab(CursorBundle);
 
-    let material = materials.add(ColorMaterial::from(cursor.color));
-
+fn cursors_setup(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let sprite_handle = asset_server.load("cursor/cursor.png");
     let bundle = CursorBundle {
         cursor: CursorComponent {},
-        mesh_bundle: MaterialMesh2dBundle {
-            mesh: mesh_handle.into(),
-            material,
-            transform: Transform::from_xyz(cursor.x, cursor.y, -1.0),
+        sprite_bundle: SpriteBundle {
+            texture: sprite_handle,
             ..Default::default()
         },
     };
-
-    let entity = commands.spawn(bundle).id();
-    cursor_map.0.insert(player_id, entity);
+    commands.insert_resource(CursorPrefab(bundle));
 }
 
 #[derive(Resource)]
@@ -117,29 +87,23 @@ fn player_cursor_moved(
     mut cursor_moved_events: EventReader<PlayerCursorMovedEvent>,
     mut cursor_map: ResMut<CursorMap>,
     mut cursor_query: Query<&mut Transform, With<CursorComponent>>,
-    puzzle: Res<Puzzle>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    cursor_prefab: Res<CursorPrefab>,
     mut commands: Commands,
 ) {
     for event in cursor_moved_events.iter() {
+        let new_translation = Vec3::new(event.cursor.x, event.cursor.y, CURSOR_HEIGHT);
+
         if let Some(player_id) = event.player_id {
             if let Some(entity) = cursor_map.0.get(&player_id) {
                 if let Ok(mut transform) = cursor_query.get_mut(*entity) {
-                    transform.translation.x = event.cursor.x;
-                    transform.translation.y = event.cursor.y;
-                    transform.translation.z = CURSOR_HEIGHT;
+                    transform.translation = new_translation;
                 }
             } else {
-                add_cursor(
-                    &event.cursor,
-                    player_id,
-                    puzzle.as_ref(),
-                    meshes.as_mut(),
-                    materials.as_mut(),
-                    &mut cursor_map,
-                    &mut commands,
-                )
+                let mut bundle = cursor_prefab.0.clone();
+                bundle.sprite_bundle.transform.translation = new_translation;
+                bundle.sprite_bundle.sprite.color = event.cursor.color;
+                let entity = commands.spawn(bundle).id();
+                cursor_map.0.insert(player_id, entity);
             }
         }
     }
@@ -177,8 +141,7 @@ fn mouse_moved(
 }
 
 fn cursor_party(
-    cursor_query: Query<&Handle<ColorMaterial>, With<CursorComponent>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut cursor_query: Query<&mut Sprite, With<CursorComponent>>,
     time: Res<Time>,
     puzzle_complete: Res<PuzzleComplete>,
 ) {
@@ -188,9 +151,9 @@ fn cursor_party(
 
     let hue = time.elapsed_seconds() * 200.0 % 360.0;
     let new_color = Color::hsl(hue, 0.8, 0.5);
-    for handle in cursor_query.iter() {
-        if let Some(mut material) = materials.get_mut(handle) {
-            material.color = new_color;
-        }
+    for mut sprite in cursor_query.iter_mut() {
+        sprite.color = new_color;
     }
 }
+
+fn cursor_resize() {}
